c3696858a1f471273f9537a91078c11036efa127
SARANYA JEEVA <saranyajeevaece@gmail.com>
2018-02-09 15:59:37 +0530

    Add files via upload

---
+++ Admission/admission-procedure.php
@@ -0,0 +1,137 @@
+<?php 
+include_once '../view/header.php'
+ ?>
+ 
+<main class="content-offset">
+    <section class="hakuna-common-banner hakuna-common-banner--analytics" style="background-image:url('../shanmugha/admission/admission-procedure.jpg');">
+        <!-- <div class="hakuna-common-banner__overlay"></div> -->
+        <div class="container-inner hakuna-common-banner--title">
+            <h3 data-aos="fade-right" data-aos-duration="1200" class="banner-heading hakuna-common-banner__heading">Admission Procedures</h3>
+        </div>
+    </section>
+    <section class="hakuna-common-banner-description">
+        <div class="container-inner">
+            <p class="hakuna-common-banner-description__content">Candidates are advised to apply in the separate prescribed application forms for UG and PG programmes.</p>
+        </div>
+    </section>
+    <section class="approach-flow-section fixed-body">
+        <div class="container-inner">
+            <div class="approach-flow">
+                <h3 class="approach-flow__heading">Under-Graduate Programmes</h3>
+                    <p>Application for admission shall be made in the prescribed form. The form can be obtained from the College Office or download from given link. Separate application shall be made for each course.</p>
+                     <p>B.E/B.Tech degree course will be completed over a period of 4 years consisting of 8 semesters.</p>
+            </div>
+            <div class="approach-flow">
+                <h3 class="approach-flow__heading">Submission of Application</h3>
+                <p>Filled in application forms with</p>
+                    <ul class="u-list">
+                        <li class="u-list__item">Passport Size Photos (2 Copies)</li>
+                        <li class="u-list__item">Photocopy of the SSLC Mark sheet</li>
+                        <li class="u-list__item">Photocopy of the +2 mark statement</li>
+                        <li class="u-list__item">Photocopy of the TC</li>
+                        <li class="u-list__item">Photocopy of the Community Certificate</li>
+                        <li class="u-list__item">Photocopy of the Conduct Certificate</li>
+
+                    </ul><br><br>
+                <p>shall be submitted to <b>The Principal, Sri Shanmugha College of Engineering and Technology Tiruchengode – Sankari Main Road, Pullipalayam, Morur(Po), Sankari (Tk), Salem (Dt) – 637 304.</b></p>
+                <p>Admission will be made in each branch according to the rules of reservation prescribed by the Government of Tamil Nadu.</p>
+                <p>Direct admission will be done under Management Quota. The candidates can directly approach the Principal from the next day of the publication of +2 results with a printout of marks from internet.</p>
+                <p>Click here to download Application <b>[maxbutton id="9"]</b></p>
+            </div>
+            <div class="approach-flow">
+                <h3 class="approach-flow__heading">Under-Graduate Programmes (Lateral Entry)</h3>
+                <p>Candidates who have passed Diploma in Engineering awarded by the State Board of Technical Education or Equivalent after undergoing regular course of three years study in an approved institution are eligible for admission.</p>
+                <p>B.E/B.Tech degree course will be completed over a period of 3 years consisting of 6 semesters.</p>
+                <p>Application for admission shall be made in the prescribed form. The form can be obtained from the College Office or download from given link. Separate application shall be made for each course.</p>
+                
+            </div>
+            <div class="approach-flow">
+                <h3 class="approach-flow__heading">Submission of Application</h3>
+                <p>Filled in application forms with</p>
+                <ul class="u-list">
+                    <li class="u-list__item">Passport Size Photos (2 Copies)</li>
+                    <li class="u-list__item">Photocopy of the SSLC Mark sheet</li>
+                    <li class="u-list__item">Photocopy of the Polytechnic TC</li>
+                    <li class="u-list__item">Photocopy of the 5th & 6th Semester Mark sheets [Revaluation Mark sheet (if any)]</li>
+                    <li class="u-list__item">Photocopy of the Community Certificate</li>
+                    <li class="u-list__item">Photocopy of the Provisional Certificate</li>
+                    <li class="u-list__item">Photocopy of the Conduct Certificate</li>
+                    
+                </ul><br><br>
+                <p>shall be submitted to <b>The Principal, Sri Shanmugha College of Engineering and Technology Tiruchengode – Sankari Main Road, Pullipalayam, Morur(Po), Sankari (Tk), Salem (Dt) – 637 304.</b></p>
+                <p>Click here to download Application <b>[maxbutton id="10"]</b></p>
+            </div>
+            <div class="approach-flow">
+                <h3 class="approach-flow__heading">Post-Graduate Programmes</h3>
+                <p>Application for admission shall be made in the prescribed form. The form can be obtained from the College Office or download from given link. Separate application shall be made for each course.</p>
+                <p>M.E/M.Tech degree course will be completed over a period of 2 years consisting of 4 semesters.</p>
+                
+            </div>
+            <div class="approach-flow">
+                <h3 class="approach-flow__heading">Submission of Application</h3>
+                <p>Filled in application forms with</p>
+                <ul class="u-list">
+                    <li class="u-list__item">Passport Size Photos (2 Copies)</li>
+                    <li class="u-list__item">Photocopy of the SSLC Mark sheet</li>
+                    <li class="u-list__item">Photocopy of the +2 Mark sheet</li>
+                    <li class="u-list__item">Photocopy of the Semester wise (not consolidated) Mark statements of the Degree Programme</li>
+                    <li class="u-list__item">Photocopy of the Community Certificate</li>
+                    <li class="u-list__item">Photocopy of the Provisional Certificate</li>
+                    <li class="u-list__item">Photocopy of the Conduct Certificate</li>
+                    
+                </ul><br><br>
+                <p>shall be submitted to <b>The Principal, Sri Shanmugha College of Engineering and Technology Tiruchengode – Sankari Main Road, Pullipalayam, Morur(Po), Sankari (Tk), Salem (Dt) – 637 304.</b></p>
+                <p>Click here to download Application <b>[maxbutton id="11"]</b></p>
+                <p>All admissions are subjected to verification of the Mark Statements by the Director of Government Examination and if the Mark Statement is found to be bogus, the admission is liable to be cancelled and criminal action would be taken against the concerned candidate.</p>
+            </div>
+            <div class="approach-flow">
+                <h3 class="approach-flow__heading">Certificates to be Produced at the Time of Admission</h3>
+                <ul class="u-list">
+                    <li class="u-list__item">10th & 12th standard mark statements. (Original and Photo copies)</li>
+                    <li class="u-list__item">Transfer Certificate. (Original and Photo copy)</li>
+                    <li class="u-list__item">Conduct Certificate from the Head of the Institution last studied (Original)</li>
+                    <li class="u-list__item">Community Certificate (Original) if the selection is based on Community Quota (not applicable for Open Competition selection)</li>
+                    <li class="u-list__item">NCC/NSS Certificate in the prescribed form</li>
+                    <li class="u-list__item">2 stamp size photos</li>
+                    <li class="u-list__item">Eligibility certificate from the Recognized University for the Candidates who passed qualifying examination other than +2 examination of Tamil Nadu Higher Secondary / CBSE.</li>
+                    <li class="u-list__item">Degree Marks Statements (Semester wise original)</li>
+                    <li class="u-list__item">Provision certificate from the concerned university incase of PG admission/Lateral Entry</li>
+                    <li class="u-list__item">Diploma 5th & 6th Semester mark statements incase of Lateral Entry (Original and Photo copies)</li>  
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!-- <section class="process-flow">
+        <div class="container-inner">
+            <h2 class="section-heading process-flow__heading">PROCESS</h2><img src="../wp-content/uploads/2017/01/approach-process-flow-1.png" alt="Process flow" class="process-flow__image" />
+        </div>
+    </section>
+    <section class="process-flow process-flow__testing">
+        <div class="container-inner">
+            <h2 class="section-heading process-flow__heading">TESTING</h2><img src="../wp-content/uploads/2017/01/approach-testing-flow-1.png" alt="Testing flow" class="process-flow__image" />
+        </div>
+    </section>
+    <section class="other-services-list-section">
+        <div class="container-inner">
+            <h2 data-aos="fade-down" data-aos-duration="1200" class="section-heading other-services-list-section--heading">More about hakunamatata</h2>
+            <ul class="other-services-list">
+                <li class="other-services-list-item">
+                    <a href="../testimonials/index.html" class="other-services-list__link">
+                        <object id="svg-credit" type="image/svg+xml" data="../wp-content/uploads/2017/01/credit-1.svg" class="other-services-list-item__image"></object><span class="other-services-list-item__label">OUR CREDITS</span></a>
+                </li>
+                <li class="other-services-list-item">
+                    <a href="http://www.wp.techaspirants.com/hakuna/our-leadership/" class="other-services-list__link">
+                        <object id="svg-leadership" type="image/svg+xml" data="../wp-content/uploads/2016/12/leadership.svg" class="other-services-list-item__image"></object><span class="other-services-list-item__label">OUR LEADERSHIP</span></a>
+                </li>
+                <li class="other-services-list-item">
+                    <a href="http://www.wp.techaspirants.com/hakuna/our-team/" class="other-services-list__link">
+                        <object id="svg-people" type="image/svg+xml" data="../wp-content/uploads/2016/12/people.svg" class="other-services-list-item__image"></object><span class="other-services-list-item__label">OUR TEAM</span></a>
+                </li>
+            </ul>
+        </div>
+    </section> -->
+</main>
+<?php 
+include_once '../view/footer.php'
+ ?>
+<script src="../wp-content/themes/techaspirants/js/approach-svg-icon-anim.js"></script>
---
+++ Admission/download-app.php
@@ -0,0 +1,25 @@
+<?php 
+include_once '../view/header.php'
+ ?>
+ 
+<main class="content-offset">
+    <section class="hakuna-common-banner hakuna-common-banner--analytics" style="background-image:url('../shanmugha/admission/download.jpg');">
+        <!-- <div class="hakuna-common-banner__overlay"></div> -->
+        <div class="container-inner hakuna-common-banner--title">
+            <h3 data-aos="fade-right" data-aos-duration="1200" class="banner-heading hakuna-common-banner__heading">Download Applications</h3>
+        </div>
+    </section>
+    <section class="hakuna-common-banner-description">
+        <div class="container-inner">
+            <p class="hakuna-common-banner-description__content" >
+            <a href="http://shanmugha.edu.in//wp-content/uploads/2014/05/UG-application-form.pdf" target="+blank"><u>Application for Under Graduate Degrees</u></a></p>
+            <p class="hakuna-common-banner-description__content" ><a href="http://shanmugha.edu.in//wp-content/uploads/2014/05/M.E-Application.pdf" target="+blank"><u>Application for Post Graduate Degrees</u></a></p>
+        </div>
+    </section>
+    
+   
+</main>
+<?php 
+include_once '../view/footer.php'
+ ?>
+<script src="../wp-content/themes/techaspirants/js/approach-svg-icon-anim.js"></script>
